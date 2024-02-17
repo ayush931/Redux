603a0b764d7f56aa2c3105c61207bc2c684d6022
ayush931 <ayushkumar9315983@gmail.com>
2024-02-18 00:21:38 +0530

    More redux Apis

---
+++ createStore.js
@@ -0,0 +1,45 @@
+import { createStore, bindActionCreators } from "redux";
+
+const ADD_TODO = 'add_todo'
+const DEL_TODO = 'delete_todo'
+const UPD_TODO = 'edit_todo'
+
+function todoReducer(state, action) {
+    if (action.type == ADD_TODO) {
+        const todoText = action.payload.todoText
+        return [
+            ...state,
+            {text: todoText, isFinished: false, id: (state.length == 0) ? 1 : state[state.length - 1].id + 1}
+        ]
+    } else if(action.type == DEL_TODO) {
+        const todoId = action.payload.todoId
+        return state.filter(t => t.id != todoId)
+    } else if(action.type == UPD_TODO) {
+        const todo = action.payload.todo
+        const todoText = action.payload.todoText
+        return state.map(t => {
+            if(t.id == todo.id) {
+                t.text = todoText
+            }
+            return t
+        })
+    }
+    return state
+}
+
+// action objects -> action methods (action creator)
+
+const addTodo = (todoText) => ({type: ADD_TODO, payload: {todoText: 'todo 1'}})
+const deleteTodo = (id) => ({type: DEL_TODO, payload: {todoId: id}})
+
+const { dispatch, subscribe, getState, replaceReducer } = createStore(todoReducer, [])
+
+subscribe(() => console.log(getState()))
+
+const actions = bindActionCreators({addTodo, deleteTodo}, dispatch)
+
+actions.addTodo('todo 1')
+
+actions.addTodo('todo 2')
+
+actions.deleteTodo(1)
